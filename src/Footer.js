4bb059786d3f49da13dd7d37f85ec06b9c86ecdb
Mais Ghaith <117035615+MaisGhaith@users.noreply.github.com>
2023-05-03 10:05:16 +0300

    edit button properities

--- src/Footer.js
+++ src/Footer.js
@@ -4,61 +4,7 @@ const Footer = () => {
     return (
         <>
             <footer className="bg-dark text-center text-white">
-                {/* Grid container */}
-                <div className="container p-4 pb-0">
-                    {/* Section: Social media */}
-                    <section className="mb-4">
-                        {/* Facebook */}
-                        <a
-                            className="btn btn-outline-light btn-floating m-1"
-                            href="#!"
-                            role="button"
-                        >
-                            <i class="bi bi-facebook"></i>                        </a>
-                        {/* Twitter */}
-                        <a
-                            className="btn btn-outline-light btn-floating m-1"
-                            href="#!"
-                            role="button"
-                        >
-                            <i className="fab fa-twitter" />
-                        </a>
-                        {/* Google */}
-                        <a
-                            className="btn btn-outline-light btn-floating m-1"
-                            href="#!"
-                            role="button"
-                        >
-                            <i className="fab fa-google" />
-                        </a>
-                        {/* Instagram */}
-                        <a
-                            className="btn btn-outline-light btn-floating m-1"
-                            href="#!"
-                            role="button"
-                        >
-                            <i className="fab fa-instagram" />
-                        </a>
-                        {/* Linkedin */}
-                        <a
-                            className="btn btn-outline-light btn-floating m-1"
-                            href="#!"
-                            role="button"
-                        >
-                            <i className="fab fa-linkedin-in" />
-                        </a>
-                        {/* Github */}
-                        <a
-                            className="btn btn-outline-light btn-floating m-1"
-                            href="#!"
-                            role="button"
-                        >
-                            <i className="fab fa-github" />
-                        </a>
-                    </section>
-                    {/* Section: Social media */}
-                </div>
-                {/* Grid container */}
+
                 {/* Copyright */}
                 <div
                     className="text-center p-3"
